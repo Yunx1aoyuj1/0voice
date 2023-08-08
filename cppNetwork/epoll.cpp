e49d11a44b101ba60ba06dc63929212252733f82
“lin” <Magge_Lin@outlook.com>
2023-08-08 17:54:13 +0800

    update epoll.cpp

--- cppNetwork/epoll.cpp
+++ cppNetwork/epoll.cpp
@@ -19,15 +19,11 @@ int main(int argc, char* argv[])
 {
     if(argc < 2)
         return -1;
-    
+
     int port = atoi(argv[1]);
 
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd <= 0)
-    {
-        errlog<<"create sockfd err."<<std::endl;
-        return -1;
-    }
+
     //非阻塞IO
     bool flags = fcntl(sockfd, F_GETFL);
     fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
@@ -37,29 +33,20 @@ int main(int argc, char* argv[])
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_addr.s_addr = INADDR_ANY;
     serverAddr.sin_port = htons(port);
-    if(bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0)
-    {
-        errlog<<"bind err.sockfd:"<<sockfd<<"   port:"<<port<<std::endl;
-        return -1;
-    }
+    bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
 
-    if(listen(sockfd, 20) < 0)
-    {
-        errlog<<"listen err."<<std::endl;
-        return -1;
-    }
+    listen(sockfd, 20);
 
     errlog<<"listen port:"<<port<<std::endl;
 
     //epoll
-    int epfd = epoll_create(1);     //只需要 > 0 即可
+    int epfd = epoll_create(1);
 
     struct epoll_event ev;
     ev.data.fd = sockfd;
     ev.events = EPOLLIN;
-
     epoll_ctl(epfd, EPOLL_CTL_ADD, sockfd, &ev);
-
+    
     struct epoll_event events[EVENTS_LENGTH] = {0};
 
     struct sockaddr_in clientAddr;
@@ -67,16 +54,10 @@ int main(int argc, char* argv[])
 
     while (1)
     {
-        int nready = epoll_wait(epfd, events, EVENTS_LENGTH, -1);   //最后一个参数的含义
-
-        if(nready <= 0)
-        {
-            continue;
-        }
-
+        int nready = epoll_wait(epfd, events, EVENTS_LENGTH, -1);
         for(int i = 0; i < nready; ++i)
         {
-            if (events[i].data.fd == sockfd)
+            if(events[i].data.fd == sockfd)
             {
                 int clientfd = accept(sockfd, (struct sockaddr*)&clientAddr, &clientlen);
                 if(clientfd <= 0)
@@ -92,32 +73,35 @@ int main(int argc, char* argv[])
 
                 epoll_ctl(epfd, EPOLL_CTL_ADD, clientfd, &ev);
             }
-            else if(events[i].events & EPOLLIN)
+            else
             {
-                char buff[BUFFER_LENGTH] = {0};
-                int len = recv(events[i].data.fd, buff, BUFFER_LENGTH, 0);
-                if(len < 0)
+                int clientfd = events[i].data.fd;
+                if (events[i].events & EPOLLIN)
                 {
-                    errlog<<"recv err."<<std::endl;
-                    return -1;
+                    char buff[BUFFER_LENGTH] = {0};
+                    int len = recv(clientfd, buff, BUFFER_LENGTH, 0);
+                    if(len < 0)
+                    {
+                        errlog<<"recv err."<<std::endl;
+                        return -1;
+                    }
+                    else if(len == 0)
+                    {
+                        errlog<<"close clientfd:"<<clientfd<<std::endl;
+                        epoll_ctl(epfd, EPOLL_CTL_DEL, clientfd, &ev);
+                        close(clientfd);
+
+                        continue;
+                    }
+
+                    errlog<<"recv:"<<buff<<std::endl;
+
+                    send(clientfd, buff, len, 0);
                 }
-                else if(len == 0)
-                {
-                    errlog<<"close clientfd:"<<events[i].data.fd<<std::endl;
-                    epoll_ctl(epfd, EPOLL_CTL_DEL, events[i].data.fd, NULL);
-
-                    close(events[i].data.fd);
-
-                    continue;
-                }
-
-                errlog<<"recv:"<<buff<<std::endl;
-
-                send(events[i].data.fd, buff, len, 0);
             }
         }
     }
-    
+
     close(sockfd);
 
     return 0;
